8c49054cfdd1bea51d46a2c77a2b79bce2f0e749
TRenfro <tyler.v.renfro@gmail.com>
2017-07-14 14:52:07 -0500

    fixes social on footer, JS compiling errors, and adds copy toast to email button

--- js/main.js
+++ js/main.js
@@ -2,15 +2,5 @@ AOS.init({
   duration: 900,
 })
 
-document.getElementById("nav-toggle").addEventListener("click", toggleNav);
-function toggleNav() {
-  var nav = document.getElementById("nav-menu");
-  var className = nav.getAttribute("class");
-  if (className == "nav-right nav-menu") {
-    nav.className = "nav-right nav-menu is-active";
-  } else {
-    nav.className = "nav-right nav-menu";
-  }
-}
-
 new Clipboard('.btn');
+
